58097a03ce925d91259954b45167615ccceba4f5
Gregory Petukhov <lorien@lorien.name>
2014-07-16 01:40:53 +0700

    Improve mine_bioguide.py script

--- mine_bioguide.py
+++ mine_bioguide.py
@@ -23,18 +23,50 @@ KNOWN_PERSONS = (
    'G000280',
 )
 
-REX_CHANGE = re.compile(r'(changed (\S+ )?party affiliation|changed from|switched to).{40}', re.I)
+REX_CHANGE = re.compile(r'(changed (\S+ )?party affiliation|changed from|switched to)[^;]+', re.I)
+REX_CHANGE_DATE = re.compile(r'\b\w+ \d+, \d{4}\b', re.I)
+REX_CHANGE_YEAR = re.compile(r'\b\d{4}\b', re.I)
 
 def main():
     members = {}
+    walkers = {
+        'known': [],
+        'new': [],
+    }
     for row in csv.reader(open('cache/bioguide3.csv')):
-        mid, bio = row
+        mid, bio = [x.decode('utf-8') for x in row]
         name = ','.join(bio.split(',', 2)[:2])
 
         match = REX_CHANGE.search(bio)
         if match:
-            if not mid in KNOWN_PERSONS:
-                print mid, name, ' -- ', match.group(0)
+            snippet = match.group(0)
+            try:
+                change_date = REX_CHANGE_DATE.search(snippet).group(0)
+            except AttributeError:
+                try:
+                    change_date = REX_CHANGE_YEAR.search(snippet).group(0)
+                except AttributeError:
+                    change_date = None
+            item = (mid, name, change_date, snippet)
+            if mid in KNOWN_PERSONS:
+                walkers['known'].append(item)
+            else:
+                walkers['new'].append(item)
+
+    print '============='
+    print 'Known walkers'
+    print '============='
+    for mid, name, date, snippet in walkers['known']:
+        date_rep = date if date is not None else 'NA'
+        print u'[%s] %s (%s) -- %s' % (mid, name, date_rep, snippet)
+
+
+    print
+    print '=============='
+    print 'Unnown walkers'
+    print '=============='
+    for mid, name, date, snippet in walkers['known']:
+        print u'[%s] %s (%s) -- %s' % (mid, name, date, snippet)
 
 
 if __name__ == '__main__':
